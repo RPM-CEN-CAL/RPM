c0a84470b4d6dcc9393c68aa75154700757fb8cc
KraziCali99 <your_email@example.com>
2026-08-22 16:32:40 -0700

    Fix syntax error in server.js app.listen

--- server.js
+++ server.js
@@ -32,4 +32,6 @@ app.post('/api/create-square-checkout', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(Server running on port ));
+app.listen(PORT, () => {
+  console.log('Server running on port ' + PORT);
+});
